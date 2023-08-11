06e5bb3c7ed348a1fcc971dd0d4c9df31aa3127e
xta <rexfeng@gmail.com>
2023-08-10 20:31:06 -0700

    add super basic about path/page

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+export default function About() {
+
+    return (
+        <>
+            <div><Link href="/">Home</Link></div>
+            <div>About</div>
+        </>
+    )
+
+}
