cb57f03b9f663e078a485bdade2d6fb2277f2cb8
xta <rexfeng@gmail.com>
2023-08-10 21:23:09 -0700

    create new AboutContext for passing input text to <Item />

--- src/app/about/Item.js
+++ src/app/about/Item.js
@@ -1,10 +1,16 @@
 import { useContext } from 'react';
 import { LevelContext } from '@/components/LevelContext'
+import { AboutContext } from './AboutContext';
 
 export default function Item() {
     let level = useContext(LevelContext)
+    let name = useContext(AboutContext)
+
+    if (name === '') {
+        return <span><em>[Item, get level: {level}]</em></span>
+    }
 
     return (
-        <span><em>[Item, get level: {level}]</em></span>
+        <span><em>[Item, get level: {level}, get name: {name}]</em></span>
     )
 }
--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,14 +1,24 @@
 "use client"
 
+import { useState } from "react"
+
 import Link from "next/link"
 import styles from './about.module.css'
 
 import Heading from '@/components/Heading'
 import Section from "@/components/Section"
 
+import { AboutContext } from "./AboutContext"
+
 import Item from './Item'
 
 export default function About() {
+
+    const [name, setName] = useState('')
+
+    function handleName(event) {
+        setName(event.target.value)
+    }
 
     return (
         <>
@@ -16,26 +26,35 @@ export default function About() {
                 <div><Link href="/">Home</Link></div>
                 <div>About</div>
 
+                <div>
+                    <label>
+                        Enter name{' '}
+                        <input value={name} onChange={(e) => handleName(e)} placeholder="Username"></input>
+                    </label>
+                </div>
+
                 <hr className={styles.line} />
 
-                <Section level={1}>
-                    <Heading>Title <Item /></Heading>
-                    <Section level={2}>
-                        <Heading>Heading <Item /></Heading>
-                        <Heading>Heading</Heading>
-                        <Heading>Heading</Heading>
-                        <Section level={3}>
-                            <Heading>Sub-heading</Heading>
-                            <Heading>Sub-heading <Item /></Heading>
-                            <Heading>Sub-heading</Heading>
-                            <Section level={4}>
-                                <Heading>Sub-sub-heading</Heading>
-                                <Heading>Sub-sub-heading</Heading>
-                                <Heading>Sub-sub-heading <Item /></Heading>
+                <AboutContext.Provider value={name}>
+                    <Section level={1}>
+                        <Heading>Title <Item /></Heading>
+                        <Section level={2}>
+                            <Heading>Heading <Item /></Heading>
+                            <Heading>Heading</Heading>
+                            <Heading>Heading</Heading>
+                            <Section level={3}>
+                                <Heading>Sub-heading</Heading>
+                                <Heading>Sub-heading <Item /></Heading>
+                                <Heading>Sub-heading</Heading>
+                                <Section level={4}>
+                                    <Heading>Sub-sub-heading</Heading>
+                                    <Heading>Sub-sub-heading</Heading>
+                                    <Heading>Sub-sub-heading <Item /></Heading>
+                                </Section>
                             </Section>
                         </Section>
                     </Section>
-                </Section>
+                </AboutContext.Provider>
             </div>
         </>
     )
